0d36e06e7dd0a425e3f74574fc51551f78bd7e1c
maxivasil <mcalderonv@gmail.com>
2025-03-24 17:17:06 -0300

    Implements primes.c and xargs.c solutions

--- xargs.c
+++ xargs.c
@@ -1,11 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
 #ifndef NARGS
 #define NARGS 4
 #endif
 
+void
+execute_command(char *args[], int count)
+{
+	args[count + 1] = NULL;
+
+	pid_t pid = fork();
+	if (pid == 0)
+	{
+		execvp(args[0], args);
+		perror("execvp");
+		exit(EXIT_FAILURE);
+	} else if (pid > 0)
+	{
+		wait(NULL);
+	} else
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+
+	for (int i = 1; i <= count; i++)
+	{
+		free(args[i]);
+	}
+}
+
 int
 main(int argc, char *argv[])
 {
-	// Your code here
+	if (argc < 2)
+	{
+		fprintf(stderr, "Uso: %s <comando>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
 
-	return 0;
+	char *args[NARGS + 2];
+	args[0] = argv[1];
+
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	int count = 0;
+
+	while ((nread = getline(&line, &len, stdin)) != -1)
+	{
+		line[nread - 1] = '\0';
+		args[count + 1] = strdup(line);
+		if (args[count + 1] == NULL)
+		{
+			perror("strdup");
+			free(line);
+			return EXIT_FAILURE;
+		}
+		
+		count++;
+
+		if (count == NARGS)
+		{
+			execute_command(args, count);
+			count = 0;
+		}
+	}
+
+	if (count > 0)
+	{
+		execute_command(args, count);
+	}
+
+	free(line);
+	return EXIT_SUCCESS;
 }
